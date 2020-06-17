c5b51437b924642bdc570588eb79a6daf578621d
Ayeshcode <test@gmail.com>
2020-06-18 00:32:41 +0530

    Cretae public MainController

--- PracticalTest/Controllers/MainController.cs
+++ PracticalTest/Controllers/MainController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PracticalTest.Models;
 
 namespace PracticalTest.Controllers
 {
     public class MainController : Controller
+
     {
+        SMSContext ORM = null;
+         public MainController(SMSContext _ORM)
+        {
+            ORM = _ORM;
+        }
+
         public IActionResult Index()
         {
             return View();
